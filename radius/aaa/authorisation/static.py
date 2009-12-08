b688671dc46ee935f7b61e3fc7aa859e15cc2855
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2009-12-08 13:56:21 +0000

    remove the radius code - will do a separate radius

--- radius/aaa/authorisation/static.py
+++
@@ -1,7 +0,0 @@
-from __future__ import with_statement
-from radius.aaa.connections.authorisation import Authorisation
-
-
-class StaticAuthorisation(Authorisation):
-	def search(self, request):
-		return self.authorisation
